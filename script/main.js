7d268277f83e47a0e6f7989ea284acd9f66d70cb
Francisco García <frangaren@usal.es>
2018-03-30 01:44:07 +0200

    Add remote querying code

--- script/main.js
+++ script/main.js
@@ -1,14 +1,95 @@
-function Controller() {
-  var feed;
+/*
+ *  MODEL
+ */
+function Item() {
+  this.render = function () {
+    var article = document.createElement('article');
+    var header = document.createElement('header');
+    var h2 = document.createElement('h2');
+    var titleNode = document.createTextNode(this.title);
+    h2.appendChild(titleNode);
+    header.appendChild(h2);
+    article.append(header);
+    var contentSection = document.createElement('section');
+    var descriptionNode = document.createTextNode(this.descriptionNode);
+    contentSection.append(descriptionNode);
+    article.append(contentSection);
+    return article;
+  }
+}
 
-  this.loadFeed = function (url) {
-    return null;
+Item.fromRSS = function (xml) {
+  var title = xml.getElementsByTagName('title')[0].nodeValue;
+  var description = xml.getElementsByTagName('description')[0].nodeValue;
+  var item = new Item();
+  item.title = title;
+  item.description = description;
+  return item;
+}
+
+function Channel(title, description, link) {
+  this.title = title;
+  this.description = description;
+  this.link = link;
+  this.items = [];
+}
+
+Channel.fromRSS = function (xml) {
+  var xchannel = xml.getElementsByTagName('channel')[0];
+  var title = channel.getElementsByTagName('title')[0].nodeValue;
+  var description = channel.getElementsByTagName('description')[0].nodeValue;
+  var link = channel.getElementsByTagName('link')[0].nodeValue;
+  var channel = new Channel(title, description, link);
+  var items = channel.getElementsByTagName('item');
+  for (item of items) {
+    channel.items.push(Item.fromXML(item));
+  }
+  return channel;
+}
+
+
+
+/*
+ *  CONTROLLER
+ */
+function Controller() {
+  var feed = null;
+
+  this.loadFeed = function (url, callback, error_callback) {
+    var request = new XMLHttpRequest();
+    var controller = this;
+    request.onreadystatechanged = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          controller.parseRSS(this.responseXML);
+          callback();
+        } else {
+          error_callback(this.status);
+        }
+      }
+    };
+    request.open('GET', url, true);
+    request.send();
+  }
+
+  this.parseRSS = function (xml) {
+    feed = Channel.fromRSS(xml);
   }
 
   this.renderFeed = function () {
-    return null;
+    var articles = [];
+    for (item of channel.items) {
+      articles.push(item.render());
+    }
+    return articles;
   }
 }
+
+
+
+/*
+ *  VIEW
+ */
 
 function View(controller) {
   var feedUrlTextbox = null;
@@ -21,12 +102,20 @@ function View(controller) {
 
   this.updateFeed = function () {
     if (feedUrlTextbox.checkValidity()) {
-      controller.loadFeed(feedUrlTextbox.value);
-      var articles = controller.renderFeed();
-      for (article of articles) {
-        mainElement.appendChild(article);
-      }
+      controller.loadFeed(feedUrlTextbox.value, this.renderFeed,
+        this.renderError);
     }
+  }
+
+  this.renderFeed = function () {
+    var articles = controller.renderFeed();
+    for (article of articles) {
+      mainElement.appendChild(article);
+    }
+  }
+
+  this.renderError = function (error) {
+
   }
 }
 
